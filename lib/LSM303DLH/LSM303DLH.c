fe10986544956ac976a052fdb3e6a66e38c92130
Tiago Canelas <tiago.canelas@preh.pt>
2025-01-08 09:42:59 +0000

    added to LSM303DLH methods to calibrate values, method to get real values adjusted to compensation and scale, methods to get Roll and pitch angles

--- lib/LSM303DLH/LSM303DLH.c
+++ lib/LSM303DLH/LSM303DLH.c
@@ -1,63 +1,141 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
+#include <util/delay.h>
 #include "i2c.h"
 #include "LSM303DLH.h"
 
 uint8_t x_l = 0;
-uint8_t x_h = 0;
+int8_t x_h = 0;
 uint8_t y_l = 0;
-uint8_t y_h = 0;
+int8_t y_h = 0;
 uint8_t z_l = 0;
-uint8_t z_h = 0;
+int8_t z_h = 0;
 
-// Função para inicializar o acelerômetro
+float GsComp = 4096.0; // Para o caso de _G
+
 void LSM303_Accel_Init(void)
 {
     // Configura o registrador CTRL_REG1_A: habilita X, Y, Z e configura frequência de 50 Hz
     // uint8_t data = 0x27; // 0b00100111: Power on, 50Hz, enable XYZ
     I2C_WriteRegister(LSM303_ACCEL_ADDRESS, CTRL_REG1_A, 0x27);
-     I2C_WriteRegister(LSM303_ACCEL_ADDRESS,35, 0x32);
+    I2C_WriteRegister(LSM303_ACCEL_ADDRESS, 35, 0x20);
 }
 
-// Função para inicializar o magnetômetro
 void LSM303_Mag_Init(void)
 {
-    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x02, 0x00);// Single-conversion mode
-    I2C_WriteRegister(LSM303_MAG_ADDRESS, CRA_REG_M, 0x14); //30hz output
-    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x01, 0x20);//gain minimum
-    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x0A, 0x48);//Datasheet register
-    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x0B, 0x52);//Datasheet register
-    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x0C, 0x33);//Datasheet register       
+    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x02, 0x00);      // Single-conversion mode
+    I2C_WriteRegister(LSM303_MAG_ADDRESS, CRA_REG_M, 0x14); // 30hz output
+    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x01, 0x20);      // gain minimum
+    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x0A, 0x48);      // Datasheet register
+    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x0B, 0x52);      // Datasheet register
+    I2C_WriteRegister(LSM303_MAG_ADDRESS, 0x0C, 0x33);      // Datasheet register
 }
 
-// Função para ler dados do acelerômetro
 void LSM303_ReadAccel(int16_t *x, int16_t *y, int16_t *z)
 {
     x_l = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_X_L_A);
     x_h = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_X_H_A);
+
     y_l = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Y_L_A);
     y_h = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Y_H_A);
+
     z_l = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Z_L_A);
     z_h = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Z_H_A);
 
-    // Combina os bytes alto e baixo
     *x = ((x_h << 8) | x_l);
     *y = ((y_h << 8) | y_l);
     *z = ((z_h << 8) | z_l);
 }
 
-// Função para ler dados do magnetômetro
-void LSM303_ReadMag(uint16_t *x, uint16_t *y, uint16_t *z)
+void LSM303_ReadMag(int16_t *x, int16_t *y, int16_t *z)
 {
     x_l = I2C_ReadRegister(LSM303_MAG_ADDRESS, OUT_X_L_M);
     x_h = I2C_ReadRegister(LSM303_MAG_ADDRESS, OUT_X_H_M);
+
     y_l = I2C_ReadRegister(LSM303_MAG_ADDRESS, OUT_Y_L_M);
     y_h = I2C_ReadRegister(LSM303_MAG_ADDRESS, OUT_Y_H_M);
+
     z_l = I2C_ReadRegister(LSM303_MAG_ADDRESS, OUT_Z_L_M);
     z_h = I2C_ReadRegister(LSM303_MAG_ADDRESS, OUT_Z_H_M);
 
-    // Combina os bytes alto e baixo
     *x = ((x_h << 8) | x_l);
     *y = ((y_h << 8) | y_l);
     *z = ((z_h << 8) | z_l);
+}
+
+float getRealXAcc()
+{
+    x_l = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_X_L_A);
+    x_h = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_X_H_A);
+    uint16_t x = ((x_h << 8) | x_l);
+
+    return ((float)x / GsComp) - AccCalibX;
+}
+
+float getRealYAcc()
+{
+    y_l = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Y_L_A);
+    y_h = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Y_H_A);
+    uint16_t y = ((y_h << 8) | y_l);
+
+    return ((float)y / GsComp) - AccCalibY;
+}
+
+float getRealZAcc()
+{
+    z_l = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Z_L_A);
+    z_h = I2C_ReadRegister(LSM303_ACCEL_ADDRESS, OUT_Z_H_A);
+    uint16_t z = ((z_h << 8) | z_l);
+
+    return ((float)z / GsComp) - AccCalibZ;
+}
+
+float accelRoll()
+{
+    float real_accel_Y = getRealYAcc();
+    float real_accel_X = getRealXAcc();
+    float real_accel_Z = getRealZAcc();
+
+    return atan(real_accel_Y / sqrt(real_accel_X * real_accel_X + real_accel_Z * real_accel_Z)) * 1 / (3.14 / 180);
+}
+float accelPitch()
+{
+    float real_accel_Y = getRealYAcc();
+    float real_accel_X = getRealXAcc();
+    float real_accel_Z = getRealZAcc();
+
+    return atan(real_accel_X / sqrt(real_accel_Y * real_accel_Y + real_accel_Z * real_accel_Z)) * 1 / (3.14 / 180);
+}
+
+void accelRollandPitch(float *Roll, float *Pitch)
+{
+    float real_accel_Y = getRealYAcc();
+    float real_accel_X = getRealXAcc();
+    float real_accel_Z = getRealZAcc();
+    
+    *Roll = atan(real_accel_Y / sqrt(real_accel_X * real_accel_X + real_accel_Z * real_accel_Z)) * 1 / (3.14 / 180);
+    *Pitch = atan(real_accel_X / sqrt(real_accel_Y * real_accel_Y + real_accel_Z * real_accel_Z)) * 1 / (3.14 / 180);
+}
+
+void CalibAccValues(int16_t NrOfSamples)
+{
+    // Retira antiga calibração para não acomular erro
+    AccCalibX = 0;
+    AccCalibY = 0;
+    AccCalibX = 0;
+
+    for (int16_t SampleNr = 0;
+         SampleNr < NrOfSamples;
+         SampleNr++)
+    {
+        AccCalibX += getRealYAcc();
+        AccCalibY += getRealYAcc();
+        AccCalibX += getRealYAcc();
+        _delay_ms(1);
+    }
+
+    AccCalibX /= NrOfSamples;
+    AccCalibY /= NrOfSamples;
+    AccCalibX /= NrOfSamples;
 }
--- lib/LSM303DLH/LSM303DLH.h
+++ lib/LSM303DLH/LSM303DLH.h
@@ -7,29 +7,44 @@
 #include <i2c.h>
 
 // Endereços I2C do LSM303DLH
-#define LSM303_ACCEL_ADDRESS  0x19  // Endereço do acelerômetro
-#define LSM303_MAG_ADDRESS    0x1E  // Endereço do magnetômetro
+#define LSM303_ACCEL_ADDRESS 0x19 // Endereço do acelerômetro
+#define LSM303_MAG_ADDRESS 0x1E   // Endereço do magnetômetro
 
 // Registradores do LSM303DLH
-#define CTRL_REG1_A           0x20  // Configuração do acelerômetro
-#define OUT_X_L_A             0x28  // Acelerômetro eixo X (Low byte)
-#define OUT_X_H_A             0x29  // Acelerômetro eixo X (High byte)
-#define OUT_Y_L_A             0x2A  // Acelerômetro eixo Y (Low byte)
-#define OUT_Y_H_A             0x2B  // Acelerômetro eixo Y (High byte)
-#define OUT_Z_L_A             0x2C  // Acelerômetro eixo Z (Low byte)
-#define OUT_Z_H_A             0x2D  // Acelerômetro eixo Z (High byte)
+#define CTRL_REG1_A 0x20 // Configuração do acelerômetro
+#define OUT_X_L_A 0x28   // Acelerômetro eixo X (Low byte)
+#define OUT_X_H_A 0x29   // Acelerômetro eixo X (High byte)
+#define OUT_Y_L_A 0x2A   // Acelerômetro eixo Y (Low byte)
+#define OUT_Y_H_A 0x2B   // Acelerômetro eixo Y (High byte)
+#define OUT_Z_L_A 0x2C   // Acelerômetro eixo Z (Low byte)
+#define OUT_Z_H_A 0x2D   // Acelerômetro eixo Z (High byte)
 
-#define CRA_REG_M             0x00  // Configuração do magnetômetro
-#define OUT_X_H_M             0x03  // Magnetômetro eixo X (High byte)
-#define OUT_X_L_M             0x04  // Magnetômetro eixo X (Low byte)
-#define OUT_Y_H_M             0x07  // Magnetômetro eixo Y (High byte)
-#define OUT_Y_L_M             0x08  // Magnetômetro eixo Y (Low byte)
-#define OUT_Z_H_M             0x05  // Magnetômetro eixo Z (High byte)
-#define OUT_Z_L_M             0x06  // Magnetômetro eixo Z (Low byte)
+#define CRA_REG_M 0x00 // Configuração do magnetômetro
+#define OUT_X_H_M 0x03 // Magnetômetro eixo X (High byte)
+#define OUT_X_L_M 0x04 // Magnetômetro eixo X (Low byte)
+#define OUT_Y_H_M 0x07 // Magnetômetro eixo Y (High byte)
+#define OUT_Y_L_M 0x08 // Magnetômetro eixo Y (Low byte)
+#define OUT_Z_H_M 0x05 // Magnetômetro eixo Z (High byte)
+#define OUT_Z_L_M 0x06 // Magnetômetro eixo Z (Low byte)
+
+
+float AccCalibX, AccCalibY, AccCalibZ;
 
 void LSM303_Accel_Init(void);
 void LSM303_Mag_Init(void);
-void LSM303_ReadAccel(int16_t* x, int16_t* y, int16_t* z);
-void LSM303_ReadMag(uint16_t* x, uint16_t* y, uint16_t* z);
+
+void CalibAccValues(int16_t NrOfSamples);
+
+void LSM303_ReadAccel(int16_t *x, int16_t *y, int16_t *z);
+void LSM303_ReadMag(int16_t *x, int16_t *y, int16_t *z);
+
+float getRealXAcc();
+float getRealYAcc();
+float getRealZAcc();
+
+float accelRoll ();
+float accelPitch ();
+void accelRollandPitch(float *Roll, float *Pitch);
+
 
 #endif
